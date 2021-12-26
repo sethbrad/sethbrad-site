75beaaea6ff869a0c8c4c095e9a61314bc9f6668
Seth Braddock <sethbrad99@gmail.com>
2021-12-26 16:14:48 -0600

    styling changes

--- footer.php
+++ footer.php
@@ -1,20 +1,17 @@
 <div>
-    <nav class="footer navbar navbar-expand-lg navbar-light bg-light">
-        <link rel="stylesheet" href="css/footer.css">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a href="https://www.linkedin.com/in/seth-braddock099/" style="text-decoration: none;">LinkedIn</a>
-                    </li>
-                    <li class="nav-item">
-                        Phone: 319-201-4047
-                    </li>
-                    <li class="nav-item">
-                        <a href="mailto:sethbrad@iastate.edu" style="text-decoration: none;">Email: sethbrad@iastate.edu</a>
-                    </li>
-                </ul>
-            </div>
+    <nav>
+        <div>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/in/seth-braddock099/" style="text-decoration: none;">LinkedIn</a>
+                </li>
+                <li class="nav-item">
+                    Phone: 319-201-4047
+                </li>
+                <li>
+                    <a href="mailto:sethbrad@iastate.edu" style="text-decoration: none;">Email: sethbrad@iastate.edu</a>
+                </li>
+            </ul>
         </div>
     </nav>
 </div>
--- header.php
+++ header.php
@@ -2,68 +2,31 @@
 <html lang="en">
 
 <head>
-    <script>
-        function includeHTML() {
-            var z, i, elmnt, file, xhttp;
-            /* Loop through a collection of all HTML elements: */
-            z = document.getElementsByTagName("*");
-            for (i = 0; i < z.length; i++) {
-                elmnt = z[i];
-                /*search for elements with a certain atrribute:*/
-                file = elmnt.getAttribute("w3-include-html");
-                if (file) {
-                    /* Make an HTTP request using the attribute value as the file name: */
-                    xhttp = new XMLHttpRequest();
-                    xhttp.onreadystatechange = function() {
-                        if (this.readyState == 4) {
-                            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                            /* Remove the attribute, and call this function once more: */
-                            elmnt.removeAttribute("w3-include-html");
-                            includeHTML();
-                        }
-                    }
-                    xhttp.open("GET", file, true);
-                    xhttp.send();
-                    /* Exit the function: */
-                    return;
-                }
-            }
-        }
-    </script>
 
     <title>Seth Braddock's Portfolio</title>
+    <link rel="stylesheet" href="./css/index.css"
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link href="./css/index.css" rel="stylesheet">
 </head>
 
 <div>
     <h1>Seth Braddock's Portfolio</h1>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= $_SERVER['PHP_SELF'] ?>?page=about">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= $_SERVER['PHP_SELF'] ?>?page=projects">Projects</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= $_SERVER['PHP_SELF'] ?>?page=reflection">Reflections</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?= $_SERVER['PHP_SELF'] ?>?page=resources">Resources</a>
-                    </li>
-                </ul>
-            </div>
+    <nav>
+        <div>
+            <ul>
+                <li>
+                    <a href="<?= $_SERVER['PHP_SELF'] ?>?page=about">Home</a>
+                </li>
+                <li>
+                    <a href="<?= $_SERVER['PHP_SELF'] ?>?page=projects">Projects</a>
+                </li>
+                <li>
+                    <a href="<?= $_SERVER['PHP_SELF'] ?>?page=reflection">Reflections</a>
+                </li>
+                <li>
+                    <a href="<?= $_SERVER['PHP_SELF'] ?>?page=resources">Resources</a>
+                </li>
+            </ul>
         </div>
     </nav></br>
 </div>
